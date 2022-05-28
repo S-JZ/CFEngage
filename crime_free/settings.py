6aa3979a40c43e5ffbe0b55e6c49e8f5a72d752f
Sristi <sristi0108@gmail.com>
2022-05-28 23:02:16 +0530

    Updated templates part 1

--- crime_free/settings.py
+++ crime_free/settings.py
@@ -134,12 +134,16 @@ USE_I18N = True
 USE_TZ = True
 
 
+FILE_UPLOAD_HANDLERS = ['django.core.files.uploadhandler.TemporaryFileUploadHandler']
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- smart_patrol/views.py
+++ smart_patrol/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import UploadFileForm
+from django.views.decorators.csrf import ensure_csrf_cookie
 from .FaceRecognition import CamPolice
 
+
+
+@ensure_csrf_cookie
 def home(request):
-    return render(request, "index.html")
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            return redirect('cam')
+    else:
+        form = UploadFileForm()
+    return render(request, "index.html", {'form': form})
 
 
 def start_cam(request):
-    CamPolice().detect_faces()
+    file = request.FILES['file']
+    path = file.temporary_file_path()
+    CamPolice(path).process_video()
     return render(request, "index.html")
 
 
